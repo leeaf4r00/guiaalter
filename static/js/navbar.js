47df53bc64b2aacafbf06cde15a6d7be63f32165
leeaf4ar00 <96915495+leeaf4r00@users.noreply.github.com>
2024-01-20 20:04:35 -0300

    top

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,10 +1,14 @@
-// Exemplo de script para alternar a exibição da navbar em dispositivos móveis
+// static/js/navbar.js
+// Script para alternar a exibição da navbar em dispositivos móveis
 document.addEventListener("DOMContentLoaded", function () {
     const navbarToggler = document.querySelector(".navbar-toggler");
-    const navbarMenu = document.querySelector("#navbarNav");
+    const navbarMenu = document.querySelector(".navbar-nav");
 
     navbarToggler.addEventListener("click", function () {
-        // Toggle a visibilidade do menu
+        // Alternar a visibilidade do menu
         navbarMenu.classList.toggle("show");
+
+        // Opcional: Adicionar lógica para escurecer o fundo quando o menu está aberto
+        // document.body.classList.toggle('body-dimmed');
     });
 });
