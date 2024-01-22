1115e91a1d80403ff4f735d2d46d25aea8ccb28a
leeaf4ar00 <96915495+leeaf4r00@users.noreply.github.com>
2024-01-22 20:03:05 -0300

    top

--- static/js/navbar.js
+++
@@ -1,4 +0,0 @@
-// Adicione um evento de clique ao botão de hambúrguer para alternar a exibição da lista colapsável
-document.getElementById('toggleMenu').addEventListener('click', function () {
-    document.querySelector('.navbar').classList.toggle('navbar-active');
-});
